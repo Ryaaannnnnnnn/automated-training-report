395afcb0f3bc752640cae1481d2a3fbbd6725b19
Ryaaannnnnnnn <jhonryansolis10@gmail.com>
2026-04-19 15:03:28 +0800

    fix(next): whitelist Cloudinary domains for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   // Allow other devices on your LAN to load dev assets.
   allowedDevOrigins: ["192.168.1.8", "192.168.18.203"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
